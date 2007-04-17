1b47c4e3b19355b08f377faa26fef6a3ccd7db2f
Andrea Milazzo <mancausoft@edumips.org>
2007-04-17 16:32:19 +0000

    Removed System.out.println

--- core/is/DMULT.java
+++ core/is/DMULT.java
@@ -96,8 +96,6 @@ class DMULT extends ALU_RType
 		hi = tmp.substring(0,64);
 		lo = tmp.substring(64);
 
-		System.out.println("bitCount(): " + result.bitCount() +" bitLength(): "+ result.bitLength());
-	
 		if(enableForwarding)
 		{
 			doWB();
