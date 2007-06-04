ce5750b218389224672215552208eb5c1cc7b9e5
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2007-06-04 10:34:57 +0000

    Corrected print.s and SYSCALL 1 example. The others examples must be corrected too

--- docs/listings/print.s
+++ docs/listings/print.s
@@ -4,6 +4,7 @@
 params_sys5:    .space  8
 
                 .text
-print_string:   sw      \$a0, params_sys5(r0)    
+print_string:   sw      $a0, params_sys5(r0)    
                 daddi   r14, r0, params_sys5
                 syscall 5
+                jr      r31
--- docs/listings/syscall_1_example.s
+++ docs/listings/syscall_1_example.s
@@ -9,13 +9,16 @@ params_sys1:    .asciiz     "filename.txt"
                 .word64     0xF                    
 
                 .text
-                #include    print.s            
 
 open:           daddi       r14, r0, params_sys1    
                 syscall     1    
                 daddi       $s0, r0, -1
                 dadd        $s2, r0, r1        
                 daddi       $a0, r0, ok_message    
-                bne         r1 $s0, print_string    
+                bne         r1, $s0, print_string    
                 daddi       $a0, r0, error_op        
                 jal         print_string            
+                syscall     0
+
+                #include    print.s            
+
