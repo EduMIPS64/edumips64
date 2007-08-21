2912159ab744d21f1feec3c85cc0b6bb5fc52ffa
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2007-08-21 22:10:26 +0000

    Added the italian html helpset

---
+++ help/images.pl
@@ -0,0 +1,55 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/2+8=10;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="90" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$2 + 8 = 10$">|; 
+
+$key = q/-1;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="29" HEIGHT="33" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$-1$">|; 
+
+$key = q/backslashbackslash;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="24" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\backslash\backslash$">|; 
+
+$key = q/{lstlisting}[caption={Byteadiacenti},label={code:data_array},style={mips}].data.byte1,2,3,4.byte1.byte2.byte3.byte4{lstlisting};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="412" HEIGHT="171" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{lstlisting}[caption={Byte adiacenti}, label={code:data_array}, style={mips}]
+.data
+.byte 1, 2, 3, 4
+.byte 1
+.byte 2
+.byte 3
+.byte 4
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[caption={EsempiodicodiceEduMIPS64{}},label={code:sample},style={mip5;Questoe'uncommentoinlinea{par{.codedaddir1,r0,0syscall0{lstlisting};LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="598" HEIGHT="192" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{lstlisting}[caption={Esempio di codice EduMIPS64{}}, label={code:sample},...
+... e' un commento in linea
+\par
+.code
+daddi r1, r0, 0
+syscall 0
+\end{lstlisting}">|; 
+
+$key = q/backslash;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="14" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\backslash$">|; 
+
+1;
+
---
+++ help/labels.pl
@@ -0,0 +1,57 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/sys1/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:mips32-reg/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:datatypes/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/instructions/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sys5/;
+$external_labels{$key} = "$URL/" . q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sysc-man/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dialog-settings/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:escaping/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dialog-dinero/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/table:effect/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dialog-help/;
+$external_labels{$key} = "$URL/" . q|node44.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
