94e0d48814d32998edbe0502f4f15fe90cd32ff6
Massimo Trubia <max83t@virgilio.it>
2007-08-21 08:56:16 +0000

    Arranged a linux exception for the FPUConfigurator.java
    Conflicts:
    
    	edumips64-svn.jar

--- utils/FPUConfigurator.java
+++ utils/FPUConfigurator.java
@@ -1,6 +1,6 @@
 /* FPUConfigurator.java
  *
- * This class gives the current Local settings.
+ * This class gives the FPU current Local settings.
  * (c)Massimo Trubia 2007
  *
  * This file is part of the EduMIPS64 project, and is released under the GNU
@@ -22,6 +22,7 @@
  */
 package edumips64.utils;
 
+import edumips64.Main;
 import java.util.*;
 import java.io.*;
 import java.net.URL;
@@ -31,36 +32,39 @@ public class FPUConfigurator{
 		fparithmetic = new LinkedList<String>();
 		terminating = new LinkedList<String>();
 		try{
-			//the pattern to parse is   <tag>any character(at least one)</tag> any character (zero or more) 
+			//the pattern of the line to parse is    <tag>any character(at least one)</tag> any character (zero or more) 
 			loadData("I","<I>.+</I>.*",fparithmetic);
 			loadData("O","<O>.+</O>.*",terminating);
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-
-		
 	}
 	
-	public static void loadData(String tag,String regex, LinkedList<String> data)throws FileNotFoundException, IOException{
+	public static void loadData(String tag,String regex, LinkedList<String> data)throws  ConfigFileNotFoundException{
 		String line;
 		String splitted[];
-		//BufferedReader in;
+		InputStream configfile=null;
+		InputStreamReader isr=null;
 		
-		URL url = FPUConfigurator.class.getResource("FPU.properties");
-            	InputStreamReader isr = new InputStreamReader (url.openStream ());
-            	BufferedReader br = new BufferedReader (isr);
-				
-		//we add into the data list tagged valued (es.)  <tag>valueToAddIntoData</tag>
-		while ((line = br.readLine())!=null){
-			if(line.matches(regex)){
-				splitted=line.split("<"+tag+">");
-				splitted=splitted[1].split("</" + tag + ">");
-				splitted[0]=splitted[0].trim();
-				data.add(splitted[0]);
+		try {
+			URL url = FPUConfigurator.class.getResource("fpu.properties");
+			isr = new InputStreamReader (url.openStream ());
+			BufferedReader br = new BufferedReader (isr);
+					
+			//we add into the data list tagged values (es.)  <tag>valueToAddIntoData</tag>
+			while ((line = br.readLine())!=null){
+				if(line.matches(regex)){
+					splitted=line.split("<"+tag+">");
+					splitted=splitted[1].split("</" + tag + ">");
+					splitted[0]=splitted[0].trim();
+					data.add(splitted[0]);
+				}
 			}
+			br.close();
+			isr.close();			
+		} catch (IOException ex) {
+			throw new ConfigFileNotFoundException();
 		}
-		br.close();
-		isr.close();
 	}
 
 	public LinkedList<String> getFPArithmeticInstructions(){
