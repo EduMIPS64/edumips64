df4df354af614821788c35a6b66315cdb65ee15e
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2007-08-25 09:05:12 +0000

    Changed the svn version to 1.0 alpha

--- Main.java
+++ Main.java
@@ -51,7 +51,7 @@ import java.math.BigInteger;
 
 public class Main extends JApplet {
 
-    public static final String VERSION = "0.5.1";
+    public static final String VERSION = "1.0alpha";
     static CPU cpu;
     public static CPUGUIThread cgt;
     static Parser parser;
