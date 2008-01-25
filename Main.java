e71132e14d63f53d9951c140cf2f343088765e6a
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2008-01-25 20:16:47 +0000

    Updated the version in order to know that we're running SVN version

--- Main.java
+++ Main.java
@@ -51,7 +51,7 @@ import java.math.BigInteger;
 
 public class Main extends JApplet {
 
-    public static final String VERSION = "1.0alpha";
+    public static final String VERSION = "1.0alpha-svn";
     static CPU cpu;
     public static CPUGUIThread cgt;
     static Parser parser;
