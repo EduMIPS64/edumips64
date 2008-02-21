6abee195c25bba8580778e385fa1493bd30ca633
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2008-02-21 16:10:31 +0000

    Added a ParserTest class, that will be used in the development of the parser, and a new parsertest ant target, that builds a modified jar that executes the ParserTest main.

---
+++ ParserTest.java
@@ -0,0 +1,8 @@
+package edumips64;
+import edumips64.core.parser.Scanner;
+
+public class ParserTest {
+    public static void main(String[] args) throws Exception {
+        Scanner.main(args);
+    }
+}
