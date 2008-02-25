fe89581de13eff60d1b550911284f22bba28baf6
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2008-02-25 10:18:22 +0000

    Added an option to ParserTest, that allows to test the scanner alone

--- ParserTest.java
+++ ParserTest.java
@@ -5,9 +5,16 @@ import edumips64.core.parser.*;
 public class ParserTest {
     public static void main(String[] args) throws Exception {
         // Scanner.main(args);
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-        Scanner scanner = new Scanner(reader);
-        Parser p = Parser.getInstance();
-        p.parse(scanner);
+        if(args.length > 1) {
+            System.out.println("sc");
+            // Opzione!
+            Scanner.main(args);
+        }
+        else {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            Scanner scanner = new Scanner(reader);
+            Parser p = Parser.getInstance();
+            p.parse(scanner);
+        }
     }
 }
