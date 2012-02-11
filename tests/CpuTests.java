52111e9f37572bdc72d87f33254546fee5c570e2
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2012-02-11 20:31:05 +0100

    Add regression tests for issue #7
    * add the CpuTests class, that is the basic skeleton for unit tests
      related to the CPU;
    * add the tests target in the buildfile;
    * add tests for issue #7.
    * change README to document what is needed for running tests

---
+++ tests/CpuTests.java
@@ -0,0 +1,71 @@
+/* CpuTests.java
+ *
+ * Tests for the EduMIPS64 CPU.
+ *
+ * (c) 2012 Andrea Spadaccini
+ *
+ * This file is part of the EduMIPS64 project, and is released under the GNU
+ * General Public License.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package edumips64.tests;
+
+import edumips64.core.*;
+import edumips64.core.is.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+public class CpuTests {
+    protected CPU cpu;
+    protected Parser parser;
+
+    @Before
+    public void setUp() {
+        cpu = CPU.getInstance();
+        cpu.setStatus(CPU.CPUStatus.READY);
+        parser = Parser.getInstance();
+    }
+
+    /** Runs a MIPS64 test program, raising an exception if it does not succeed.
+     *
+     * @param testPath path of the test code.
+     */
+    protected void runMipsTest(String testPath) throws Exception {
+        try {
+            parser.parse(testPath);
+            cpu.setStatus(CPU.CPUStatus.RUNNING);
+            while(true) {
+                cpu.step();
+            }
+        } finally {
+            cpu.reset();
+        }
+    }
+    /* ------- REGRESSION TESTS -------- */
+    /* Issue #7 */
+    @Test(expected = HaltException.class)
+    public void testMovnIssue7() throws Exception {
+        runMipsTest("tests/data/movn-issue-7.s");
+    }
+
+    @Test(expected = HaltException.class)
+    public void testMovzIssue7() throws Exception {
+        runMipsTest("tests/data/movz-issue-7.s");
+    }
+}
--- tests/data/movn-issue-7.s
+++ tests/data/movn-issue-7.s
@@ -1,4 +1,4 @@
-; test case for MOVN and MOVZ
+; test case for MOVN
 ;
 ; checks for the regression of bug #9, in which (with forwarding disabled) if
 ; the predicate of the MOVN or MOVZ instruction is false, and the destination
@@ -9,7 +9,7 @@
 ;
 ; Test case provided by Alessandro Bucceri
 
-        .code
+    .code
     ; MOVN rd, rs, rt
     ; Move Conditional on Not Zero
     ;
@@ -21,24 +21,5 @@
     movn    r3, r2, r1      ; if (r1 != 0), r3 = r2
     
     ; expected result: r1 = 0; r2 = 1; r3 = 2
-    
-    ; flushing the pipeline
-    nop
-    nop
-    nop
-    nop
-    nop
 
-    ; MOVZ rd, rs, rt
-    ; Move Conditional on Zero
-    ;
-    ; if GPR[rt] = 0 then GPR[rd] <- GPR[rs]
-
-    daddi    r1,r0,1        ; r1=1
-    daddi    r2,r0,1        ; r2=1
-    daddi    r3,r0,2        ; r3=2
-    movz     r3,r2,r1       ; if (r1 == 0)  r3=r2;
-
-    ; expected result: r1=1, r2=1, r3=2 
-    
     syscall 0
