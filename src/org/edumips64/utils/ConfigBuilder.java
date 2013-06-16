6ee80f7b9b57c48d0835dbf625e0a3cb7b074b58
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2013-06-16 13:04:15 +0100

    In-memory ConfigStore implementation

--- src/org/edumips64/utils/ConfigBuilder.java
+++ src/org/edumips64/utils/ConfigBuilder.java
@@ -131,3 +131,74 @@ class JavaPrefsConfigStore extends ConfigStore {
     return prefs.getBoolean(key, default_value);
   }
 }
+
+class InMemoryConfigStore extends ConfigStore {
+  private static final Logger logger = Logger.getLogger(InMemoryConfigStore.class.getName());
+  private Map<String, Object> data;
+  private Map<String, Object> defaults;
+
+  public InMemoryConfigStore(Map<String, Object> defaults) {
+    this.defaults = defaults;
+    data = new HashMap<String, Object>();
+  }
+
+  public void putString(String key, String value) {
+    data.put(key, value);
+  }
+
+  public String getString(String key) {
+    if (data.containsKey(key)) {
+      return (String) data.get(key);
+    }
+
+    String default_value = "";
+
+    if (defaults.containsKey(key)) {
+      default_value = (String) defaults.get(key);
+    } else {
+      logger.warning("No default value for string configuration key " + key + ", using empty string.");
+    }
+
+    return default_value;
+  }
+
+  public void putInt(String key, int value) {
+    data.put(key, value);
+  }
+
+  public int getInt(String key) {
+    if (data.containsKey(key)) {
+      return (Integer) data.get(key);
+    }
+
+    int default_value = 0;
+
+    if (defaults.containsKey(key)) {
+      default_value = (Integer) defaults.get(key);
+    } else {
+      logger.warning("No default value for integer configuration key " + key + ", using 0.");
+    }
+
+    return default_value;
+  }
+
+  public void putBoolean(String key, boolean value) {
+    data.put(key, value);
+  }
+
+  public boolean getBoolean(String key) {
+    if (data.containsKey(key)) {
+      return (Boolean) data.get(key);
+    }
+
+    boolean default_value = false;
+
+    if (defaults.containsKey(key)) {
+      default_value = (Boolean) defaults.get(key);
+    } else {
+      logger.warning("No default value for boolean configuration key " + key + ", using false.");
+    }
+
+    return default_value;
+  }
+}
