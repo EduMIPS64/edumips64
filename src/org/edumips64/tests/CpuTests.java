EduMIPS64/edumips64

commit 888f4449557e690a5b06bae743643a5576bbada4

Halt execution in IF instead of ID
This will avoid the creation of ghost instructions in the pipeline. Fixes #50.

src/org/edumips64/core/CPU.java
@@ -620,6 +620,7 @@ public class CPU {
       if (status == CPUStatus.RUNNING) {
         if (pipe.get(PipeStatus.IF) != null) {  //rispetto a dinmips scambia le load con le IF
           try {
+            // Can change the CPU status from RUNNING to STOPPING.
             pipe.get(PipeStatus.IF).IF();
           } catch (BreakException exc) {
             breaking = 1;
@@ -629,10 +630,18 @@ public class CPU {
 
         logger.info("Moving " + pipe.get(PipeStatus.IF) + " to ID");
         pipe.put(PipeStatus.ID, pipe.get(PipeStatus.IF));
-        logger.info("Fetched new instruction " + mem.getInstruction(pc) + " into IF");
-        pipe.put(PipeStatus.IF, mem.getInstruction(pc));
-        old_pc.writeDoubleWord((pc.getValue()));
-        pc.writeDoubleWord((pc.getValue()) + 4);
+        Instruction next_if;
+        if (status != CPUStatus.STOPPING){
+           next_if = mem.getInstruction(pc);
+           logger.info("Fetched new instruction " + next_if);
+           old_pc.writeDoubleWord((pc.getValue()));
+           pc.writeDoubleWord((pc.getValue()) + 4);
+           logger.info("New Program Counter value: " + pc.toString());
+        } else {
+           next_if = null;
+        }
+        logger.info("Putting " + next_if + "in IF.");
+        pipe.put(PipeStatus.IF, next_if);
       } else {
         pipe.put(PipeStatus.ID, Instruction.buildInstruction("BUBBLE"));
       }

src/org/edumips64/core/is/HALT.java
@@ -51,9 +51,9 @@ public class HALT extends Instruction {
     } catch (IrregularStringOfBitsException e) {
       e.printStackTrace();
     }
+    CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
   }
   public void ID() throws RAWException, IrregularWriteOperationException, IrregularStringOfBitsException, TwosComplementSumException, HaltException {
-    CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
   }
 
   public void EX() throws HaltException, IrregularStringOfBitsException, IntegerOverflowException, TwosComplementSumException {

src/org/edumips64/core/is/SYSCALL.java
@@ -61,16 +61,17 @@ public class SYSCALL extends Instruction {
     } catch (IrregularStringOfBitsException e) {
       e.printStackTrace();
     }
-  }
 
-  public void ID() throws RAWException, IrregularWriteOperationException, IrregularStringOfBitsException {
-    syscall_n = params.get(0);
     logger.info("SYSCALL (" + this.hashCode() + ") n = " + syscall_n);
 
     if (syscall_n == 0) {
       logger.info("Stopping CPU due to SYSCALL (" + this.hashCode() + ")");
       CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
-    } else if ((syscall_n > 0) && (syscall_n <= 5)) {
+    }
+  }
+
+  public void ID() throws RAWException, IrregularWriteOperationException, IrregularStringOfBitsException {
+    if ((syscall_n > 0) && (syscall_n <= 5)) {
       CPU cpu = CPU.getInstance();
       Register r14 = cpu.getRegister(14);
 
@@ -84,7 +85,7 @@ public class SYSCALL extends Instruction {
       r1.incrWriteSemaphore();
       address = r14.getValue();
       logger.info("SYSCALL (" + this.hashCode() + "): locked register R14. Value = " + address);
-    } else {
+    } else if (syscall_n != 0) {
       // TODO: invalid syscall
       logger.info("INVALID SYSCALL (" + this.hashCode() + ")");
     }

commit 5180127796037183d969753cb3dff6a2d52ea8a1

Start program termination in ID rather than in IF
Fix an issue introduced in commit 888f444, in which the program termination
sequence was initiated in IF both by SYSCALL 0 and by HALT. This would cause
premature termination if the terminating instruction was fetched because of
branch misprediction. Also add 2 unit tests for this behavior.

Fixes #51

src/org/edumips64/core/CPU.java
@@ -564,6 +564,7 @@ public class CPU {
           if (fpPipe.putInstruction(pipe.get(PipeStatus.ID), true) == 0) {
             if (fpPipe.isEmpty() || (!fpPipe.isEmpty() /* && !terminatingInstructionsOPCodes.contains(pipe.get(PipeStatus.ID).getRepr().getHexString())*/)) {
               logger.info("Executing ID() for " + pipe.get(PipeStatus.ID));
+              // Can change the CPU status from RUNNING to STOPPING.
               pipe.get(PipeStatus.ID).ID();
             }
 
@@ -583,6 +584,7 @@ public class CPU {
           if (pipe.get(PipeStatus.EX) == null || /*testing*/ pipe.get(PipeStatus.EX).getName().compareTo(" ") == 0) {
             if (fpPipe.isEmpty() || (!fpPipe.isEmpty() /* && !terminatingInstructionsOPCodes.contains(pipe.get(PipeStatus.ID).getRepr().getHexString())*/)) {
               logger.info("Executing ID() for " + pipe.get(PipeStatus.ID));
+              // Can change the CPU status from RUNNING to STOPPING.
               pipe.get(PipeStatus.ID).ID();
             }
 
@@ -610,7 +612,6 @@ public class CPU {
       if (status == CPUStatus.RUNNING) {
         if (pipe.get(PipeStatus.IF) != null) {  //rispetto a dinmips scambia le load con le IF
           try {
-            // Can change the CPU status from RUNNING to STOPPING.
             logger.info("Executing IF() for " + pipe.get(PipeStatus.IF));
             pipe.get(PipeStatus.IF).IF();
           } catch (BreakException exc) {

src/org/edumips64/core/is/HALT.java
@@ -51,9 +51,9 @@ public class HALT extends Instruction {
     } catch (IrregularStringOfBitsException e) {
       e.printStackTrace();
     }
-    CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
   }
   public void ID() throws RAWException, IrregularWriteOperationException, IrregularStringOfBitsException, TwosComplementSumException, HaltException {
+    CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
   }
 
   public void EX() throws HaltException, IrregularStringOfBitsException, IntegerOverflowException, TwosComplementSumException {

src/org/edumips64/core/is/SYSCALL.java
@@ -61,15 +61,13 @@ public class SYSCALL extends Instruction {
     }
 
     logger.info("SYSCALL (" + this.hashCode() + ") n = " + syscall_n);
-
-    if (syscall_n == 0) {
-      logger.info("Stopping CPU due to SYSCALL (" + this.hashCode() + ")");
-      CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
-    }
   }
 
   public void ID() throws RAWException, IrregularWriteOperationException, IrregularStringOfBitsException {
-    if ((syscall_n > 0) && (syscall_n <= 5)) {
+    if (syscall_n == 0) {
+      logger.info("Stopping CPU due to SYSCALL (" + this.hashCode() + ")");
+      CPU.getInstance().setStatus(CPU.CPUStatus.STOPPING);
+    } else if ((syscall_n > 0) && (syscall_n <= 5)) {
       CPU cpu = CPU.getInstance();
       Register r14 = cpu.getRegister(14);
 
@@ -83,7 +81,7 @@ public class SYSCALL extends Instruction {
       r1.incrWriteSemaphore();
       address = r14.getValue();
       logger.info("SYSCALL (" + this.hashCode() + "): locked register R14. Value = " + address);
-    } else if (syscall_n != 0) {
+    } else {
       // TODO: invalid syscall
       logger.info("INVALID SYSCALL (" + this.hashCode() + ")");
     }

src/org/edumips64/tests/CpuTests.java
@@ -401,4 +401,11 @@ public class CpuTests {
   public void testNegativeAddress() throws Exception {
     runMipsTest("negative-address-issue-36.s");
   }
+
+  /* Issue #51: Problem with SYSCALL 0 after branch. */
+  @Test
+  public void testTerminationInID() throws Exception {
+    runForwardingTest("issue51-halt.s", 12, 18, 6);
+    runForwardingTest("issue51-syscall0.s", 12, 18, 6);
+  }
 }

src/org/edumips64/tests/data/issue51-halt.s
@@ -1,0 +1,5 @@
+.code
+          daddi   r2, r0, 2
+start:    daddi   r2, r2, -1
+          bne     r2, r0, start
+          halt

src/org/edumips64/tests/data/issue51-syscall0.s
@@ -1,0 +1,5 @@
+.code
+          daddi   r2, r0, 2
+start:    daddi   r2, r2, -1
+          bne     r2, r0, start
+          syscall 0