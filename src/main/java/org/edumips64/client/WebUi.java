88bcc815966158766802ca8fc71802abc5e9c879
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2016-06-19 19:25:14 +0100

    First non-working try to use JsInterop

--- src/main/java/org/edumips64/client/WebUi.java
+++ src/main/java/org/edumips64/client/WebUi.java
@@ -1,13 +1,9 @@
 package org.edumips64.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.RootPanel;
 
-import com.google.gwt.user.client.ui.TextArea;
+import jsinterop.annotations.JsType;
+
 import org.edumips64.core.CPU;
 import org.edumips64.core.Parser;
 import org.edumips64.core.is.HaltException;
@@ -16,17 +12,36 @@ import org.edumips64.utils.ConfigManager;
 import org.edumips64.utils.io.FileUtils;
 import org.edumips64.utils.io.NullFileUtils;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@JsType(namespace = "blah")
 public class WebUi implements EntryPoint {
   private CPU cpu;
   private Parser parser;
   private ConfigStore config;
   private FileUtils fu;
 
-  private TextArea code, registers, memory;
-  private Button b;
+  private final String defaultCode = ".data\n.word64 10\n.code\nlw r1, 0(r0)\nSYSCALL 0";
+
+  public String runAndGetRegisters() {
+    try {
+      cpu.reset();
+      parser.doParsing(defaultCode);
+      cpu.setStatus(CPU.CPUStatus.RUNNING);
+      while (true) {
+        cpu.step();
+      }
+    } catch (HaltException e) {
+      return cpu.gprString();
+    } catch (Exception e) {
+      return "ERROR";
+    }
+  }
 
   public void onModuleLoad() {
     // Simulator initialization.
+    Logger logger = Logger.getLogger("NameOfYourLogger");
     config = ConfigManager.getTmpConfig();
     ConfigManager.setConfig(config);
     fu = new NullFileUtils();
@@ -34,43 +49,7 @@ public class WebUi implements EntryPoint {
     parser = Parser.getInstance();
     cpu = CPU.getInstance();
 
-    // Web UI initialization.
-    code = new TextArea();
-    code.setCharacterWidth(100);
-    code.setVisibleLines(20);
-    code.setText(".data\n.word64 10\n.code\nlw r1, 0(r0)\nSYSCALL 0");
-
-    registers = new TextArea();
-    registers.setCharacterWidth(100);
-    registers.setVisibleLines(20);
-    registers.setReadOnly(true);
-
-    memory = new TextArea();
-    memory.setCharacterWidth(100);
-    memory.setVisibleLines(20);
-    memory.setReadOnly(true);
-
-    b = new Button("Run", new ClickHandler() {
-      public void onClick(ClickEvent event) {
-        try {
-          cpu.reset();
-          parser.doParsing(code.getText());
-          cpu.setStatus(CPU.CPUStatus.RUNNING);
-          while (true) {
-            cpu.step();
-          }
-        } catch (HaltException e) {
-          registers.setText(cpu.gprString());
-          memory.setText(cpu.getMemory().toString());
-        } catch (Exception e) {
-          Window.alert("PROBLEM: " + e.toString());
-        }
-      }
-    });
-
-    RootPanel.get().add(code);
-    RootPanel.get().add(b);
-    RootPanel.get().add(registers);
-    RootPanel.get().add(memory);
+    // Run the code and log registers.
+    logger.log(Level.SEVERE, runAndGetRegisters());
   }
 }
