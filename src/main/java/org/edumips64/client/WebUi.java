f778358a87941a9ac6bd92fcd7c4d54d01dc48eb
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2016-06-22 09:52:26 +0100

    Fix indentation

--- src/main/java/org/edumips64/client/WebUi.java
+++ src/main/java/org/edumips64/client/WebUi.java
@@ -62,9 +62,8 @@ public class WebUi implements EntryPoint {
         "Code Size: " + (cpu.getMemory().getInstructionsNumber() * 4) + " Bytes";
   }
 
- @Override
- public void onModuleLoad() {
- }
+  @Override
+  public void onModuleLoad() {}
 
   public void init() {
     // Simulator initialization.
