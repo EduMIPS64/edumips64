921604ab2d2c21a20bdb0af3227e0fd3993a4286
tabman83 <tabman83@gmail.com>
2016-06-29 12:15:53 +0100

    Updated gh-pages script to deploy web UI.

--- utils/update-gh-pages.sh
+++ utils/update-gh-pages.sh
@@ -13,30 +13,33 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     exit 0
   fi
 
-  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-  if [ "$GIT_BRANCH" != "master" ]; then 
-    echo "Non-master branch: $GIT_BRANCH. Not updating the JAR."
-    exit 0
-  fi
-
-  echo -n "Building JAR.. "
-  ant latest-jar > /dev/null
-  cp edumips64-latest.jar $HOME > /dev/null
-  echo "done."
-
   echo -n "Cloning git repo.. "
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lupino3/edumips64.git gh-pages > /dev/null
   echo "done."
+  
+  echo -n "Copying web UI..."
+  rm -rf gh-pages/webui  
+  mkdir gh-pages/webui
+  cp -r src/main/web/dist/* gh-pages/webui
+  
+  GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-  cd gh-pages
-  mv $HOME/edumips64-latest.jar .
+  if [ "$GIT_BRANCH" == "master" ]; then 
+    echo -n "Building JAR.. "
+    ant latest-jar > /dev/null
+    cp edumips64-latest.jar $HOME > /dev/null
+    echo "done."
+	cd gh-pages
+	mv $HOME/edumips64-latest.jar .
+  else
+    echo "Non-master branch: $GIT_BRANCH. Not updating the JAR."
+  fi
 
   # Add, commit and push files.
-  echo -n "Committing JAR.. "
+  echo -n "Committing files.. "
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
