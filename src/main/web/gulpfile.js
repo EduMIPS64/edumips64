e2cc059b388621657314859cb8b999b4a327bd47
tabman83 <tabman83@gmail.com>
2016-06-29 13:16:31 +0100

    Fix for the boilerplate.

--- src/main/web/gulpfile.js
+++ src/main/web/gulpfile.js
@@ -59,8 +59,9 @@ gulp.task('lint:test', () => {
 });
 
 gulp.task('html', ['styles', 'scripts'], () => {
-    return gulp.src('app/*.html')
+    return gulp.src('app/**/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+    .pipe($.if('*.js', $.ngAnnotate({single_quotes: true})))
     .pipe($.if('*.js', $.uglify()))
     .pipe($.if('*.css', $.cssnano({safe: true, autoprefixer: false})))
     .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
