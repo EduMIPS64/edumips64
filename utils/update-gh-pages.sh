06a18205b7536bd25356ce5d4c23b1b00170c329
Andrea Spadaccini <andrea.spadaccini@dieei.unict.it>
2016-06-29 11:57:15 +0000

    Try to fix the deploy script

--- utils/update-gh-pages.sh
+++ utils/update-gh-pages.sh
@@ -32,11 +32,29 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lupino3/edumips64.git gh-pages > /dev/null
   echo "done."
 
-  cd gh-pages
-  mv $HOME/edumips64-latest.jar .
+  echo "Git branch: $TRAVIS_BRANCH"
+
+  if [ "$TRAVIS_BRANCH" == "master" ]; then 
+    echo -n "Building JAR.. "
+    ant latest-jar > /dev/null
+    cp edumips64-latest.jar $HOME > /dev/null
+    echo "done."
+	  cd gh-pages
+	  mv $HOME/edumips64-latest.jar .
+  elif ["$TRAVIS_BRANCH" == "webui-prototype"]; then
+    echo -n "Copying web UI..."
+	  cd gh-pages
+    rm -rf webui  
+    mkdir webui
+    cp -Rf $HOME/webui/* ./webui  
+    echo "done."
+  else
+    echo "Non-master branch: $GIT_BRANCH. Not updating the JAR."
+    return 0
+  fi
 
   # Add, commit and push files.
-  echo -n "Committing JAR.. "
+  echo -n "Committing files.. "
   git add -f .
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
