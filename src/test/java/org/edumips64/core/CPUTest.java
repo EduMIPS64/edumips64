10aa9f593a4b28d0dc37afd4b6e6cee867556dab
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2016-07-16 16:25:40 +0100

    Add some basic tests for CPU

---
+++ src/test/java/org/edumips64/core/CPUTest.java
@@ -0,0 +1,34 @@
+package org.edumips64.core;
+
+import org.edumips64.BaseTest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class CPUTest extends BaseTest {
+  private Memory m;
+  private CPU cpu;
+
+  @Before
+  public void setUp() throws Exception {
+    m = new Memory();
+    cpu = new CPU(m, config);
+  }
+
+  @Test(expected = StoppedCPUException.class)
+  public void testRunningWhenStoppedThrowsException() throws Exception {
+    cpu.setStatus(CPU.CPUStatus.HALTED);
+    cpu.step();
+  }
+
+  /** This test is not very useful, but it does improve test coverage. */
+  @Test
+  public void testToString() throws Exception {
+    String cpuRepr = cpu.toString();
+    assertThat(cpuRepr, containsString("Register 0"));
+    assertThat(cpuRepr, containsString("FP Register 0"));
+    assertThat(cpuRepr, containsString("ID"));
+  }
+}
