d6308797178b208a6682c7eaa46f8acf40911c99
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2017-11-19 14:22:34 +0000

    Move images to /images in the JAR
    This build.xml change mirrors Bazel's behavior, and allows both build
    systems to produce JARs that can read images.

--- src/main/java/org/edumips64/ui/swing/img/IMGLoader.java
+++ src/main/java/org/edumips64/ui/swing/img/IMGLoader.java
@@ -3,10 +3,13 @@ import javax.imageio.ImageIO;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+
 public class IMGLoader {
   public static Image getImage(String imagename) throws IOException {
     try {
-      return ImageIO.read(IMGLoader.class.getResource(imagename));
+      URL path = IMGLoader.class.getResource("/images/" + imagename);
+      return ImageIO.read(path);
     } catch (IllegalArgumentException e) {
       // Hacky work-around to run EduMIPS64 in a debugger, out of a JAR.
       return ImageIO.read(new File("../src/org/edumips64/img/" + imagename));
