5df85a99fadf3d126cf3833bd53de41fe299476c
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2017-12-17 09:04:26 +0000

    Sort issues in the release support script

--- utils/list-mentioned-issues-since-last-release.sh
+++ utils/list-mentioned-issues-since-last-release.sh
@@ -5,7 +5,7 @@ set -e
 LAST_TAG=$(git describe --abbrev=0 --tags)
 echo "All issues since ${LAST_TAG}"
 
-ISSUES=$(git log HEAD...${LAST_TAG} | egrep "#[0-9]{1,3}" -o | sed -e "s/#//" | uniq)
+ISSUES=$(git log HEAD...${LAST_TAG} | egrep "#[0-9]{1,3}" -o | sed -e "s/#//" | sort -n | uniq)
 for issue in $ISSUES; do
   curl https://api.github.com/repos/lupino3/edumips64/issues/${issue} 2>/dev/null | jq '[.number, .title, .state] | map(tostring) | join(" - ")' | sed 's/"//g'
 done
