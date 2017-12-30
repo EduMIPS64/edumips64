230652b14dd36d52fcc3fc442ddc20b3ada1dbb8
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2017-12-30 14:46:52 +0000

    Extend pipeline tests

--- src/test/java/org/edumips64/core/PipelineTest.java
+++ src/test/java/org/edumips64/core/PipelineTest.java
@@ -1,22 +1,59 @@
 package org.edumips64.core;
 
-import org.edumips64.BaseTest;
+import org.edumips64.BaseWithInstructionBuilderTest;
 import org.edumips64.core.is.BUBBLE;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
-public class PipelineTest extends BaseTest {
+@RunWith(JUnit4.class)
+public class PipelineTest extends BaseWithInstructionBuilderTest {
   private Pipeline pipeline = new Pipeline();
+  
+  @Before
+  public void testSetup() {
+    super.testSetup();
+  }
 
   @Test
-  public void testEmptySize() {
+  public void testEmptyPipeline() {
     assertEquals(0, pipeline.size());
+    assertTrue(pipeline.isEmpty(CPU.PipeStage.IF));
+    assertTrue(pipeline.isEmpty(CPU.PipeStage.ID));
+    assertTrue(pipeline.isEmpty(CPU.PipeStage.EX));
+    assertTrue(pipeline.isEmpty(CPU.PipeStage.MEM));
+    assertTrue(pipeline.isEmpty(CPU.PipeStage.WB));
+    
+    // Test that even with an empty pipeline the getters work.
+    assertEquals(null, pipeline.IF());
+    assertEquals(null, pipeline.ID());
+    assertEquals(null, pipeline.EX());
+    assertEquals(null, pipeline.MEM());
+    assertEquals(null, pipeline.WB());
+  }
+  
+  @Test
+  public void testNonEmptySize() {
+    assertEquals(0, pipeline.size());
+    pipeline.setIF(instructionBuilder.buildInstruction("ADD"));
+    pipeline.setID(instructionBuilder.buildInstruction("ADD"));
+    assertEquals(2, pipeline.size());
   }
 
   @Test
   public void testSizeIncreaseWithBubble() {
+    assertEquals(0, pipeline.size());
     pipeline.setIF(new BUBBLE());
+    assertEquals(1, pipeline.size());
+  }
+  
+  @Test
+  public void testSizeIncreaseWithOtherInstructions() {
+    assertEquals(0, pipeline.size());
+    pipeline.setIF(instructionBuilder.buildInstruction("ADD"));
     assertEquals(1, pipeline.size());
   }
 
@@ -25,5 +62,28 @@ public class PipelineTest extends BaseTest {
     pipeline.setIF(new BUBBLE());
     assertTrue(pipeline.isBubble(CPU.PipeStage.IF));
     assertTrue(pipeline.isEmptyOrBubble(CPU.PipeStage.IF));
+    
+    pipeline.setIF(instructionBuilder.buildInstruction("ADD"));
+    assertFalse(pipeline.isBubble(CPU.PipeStage.IF));
+    assertFalse(pipeline.isEmptyOrBubble(CPU.PipeStage.IF));
+  }
+  
+  @Test
+  public void testClear() {
+    assertEquals(0, pipeline.size());
+    pipeline.setIF(new BUBBLE());
+    pipeline.setID(new BUBBLE());
+    pipeline.setEX(new BUBBLE());
+    pipeline.setMEM(new BUBBLE());
+    pipeline.setWB(new BUBBLE());
+    assertEquals(5, pipeline.size());
+    
+    pipeline.clear();
+    assertEquals(0, pipeline.size());
+    assertEquals(null, pipeline.IF());
+    assertEquals(null, pipeline.ID());
+    assertEquals(null, pipeline.EX());
+    assertEquals(null, pipeline.MEM());
+    assertEquals(null, pipeline.WB());
   }
 }
