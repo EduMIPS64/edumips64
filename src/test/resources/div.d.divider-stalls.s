f0bf4300b250d87356d2d75069e8cdfc1e4df3ea
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2018-03-20 23:15:59 +0000

    Add attribution and license to all tests.

--- src/test/resources/div.d.divider-stalls.s
+++ src/test/resources/div.d.divider-stalls.s
@@ -1,3 +1,26 @@
+; div.d.divider-stalls.s.s - test file for EduMIPS64.
+;
+; Executes two DIV.D to trigger divider stalls, and tests the results of the
+; division.
+;
+; (c) 2018 Andrea Spadaccini and the EduMIPS64 team
+;
+; This file is part of the EduMIPS64 project, and is released under the GNU
+; General Public License.
+; 
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 .code
        DADDI   r1, r0, 4
        DADDI   r2, r0, 2
--- src/test/resources/hello-world.s
+++ src/test/resources/hello-world.s
@@ -1,3 +1,25 @@
+; hello-world.s - test file for EduMIPS64.
+;
+; Prints a "Hello world" message with some interpolated strings to stdout.
+;
+; (c) 2018 Andrea Spadaccini and the EduMIPS64 team
+;
+; This file is part of the EduMIPS64 project, and is released under the GNU
+; General Public License.
+; 
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the License, or
+; (at your option) any later version.
+; 
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+; 
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, write to the Free Software
+; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
                   .data
   format_str:     .asciiz   "%dth of %s:\n%s version %i.%i is being tested! 100%% success!"
   s1:             .asciiz   "July"
