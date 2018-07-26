1a2eb641f71c481107c5f87d7183b6db3df2049e
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2018-07-26 19:07:26 +0100

    Travis: auto-deploy GWT frontend built by Bazel
    Note that now any Travis tests executed on the master branch will deploy
    to the main URL as well.
    
    Also, improve the logic with an early return in case of pull request,
    which de-indents most code by one level and build the web UI in the
    Travis tests.

--- utils/update-gh-pages.sh
+++ utils/update-gh-pages.sh
@@ -5,48 +5,62 @@ set -e
 set -u
 
 # Debug info.
-echo ${PWD}
-ls
+echo "Starting update-gh-pages.sh"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  GH_PAGES_DIR=${HOME}/gh-pages
-  echo -n "Cloning gh-pages branch to ${GH_PAGES_DIR}.. "
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lupino3/edumips64.git ${GH_PAGES_DIR} > /dev/null
-  echo "done."
-
-  echo "Git branch: $TRAVIS_BRANCH"
-
-  # Execute special per-branch actions.
-  if [ "$TRAVIS_BRANCH" == "master" ]; then 
-    echo -n "Building JAR.. "
-    ant latest-jar > /dev/null
-    echo "done."
-    cp edumips64-latest.jar ${GH_PAGES_DIR}
-  elif [ "$TRAVIS_BRANCH" == "webui-prototype" ]; then
-    echo -n "Copying web UI... "
-    mkdir -p ${GH_PAGES_DIR}/webui
-    rm -rf ${GH_PAGES_DIR}/webui/*
-    cp -Rf $HOME/webui/* ${GH_PAGES_DIR}/webui
-    echo "done."
-  fi
-
-  JS_DIR=${GH_PAGES_DIR}/edumips64/${TRAVIS_BRANCH}
+deploy_webui() {
+  JS_DIR=$1
+  WAR_PATH=bazel-bin/src/main/java/org/edumips64/
   echo "Copying the JS code to ${JS_DIR}."
   mkdir -p ${JS_DIR}
-  cp war/edumips64/* ${JS_DIR}
+  cp ${WAR_PATH}/webclient.war ${JS_DIR}
+  pushd .
+  cd ${JS_DIR}
+  unzip -o webclient.war
+  GIT_REV=$(git rev-parse HEAD)
+  echo "<br/><!--Built at Git revision ${GIT_REV}-->" >> edumips64.html
+  popd
+}
 
-  # Add, commit and push files.
-  cd ${GH_PAGES_DIR}
-  git add -f .
-  git add -f ${JS_DIR}
-  echo "Git status:"
-  git status
-  if ! git diff-index --quiet HEAD; then
-      echo -n "Committing files.. "
-      git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
-      git push -fq origin gh-pages > /dev/null
-      echo "done."
-  fi
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]; then
+  echo "Travis invoked from a pull request. Not doing any deployment step."
+  return
+fi
+
+GH_PAGES_DIR=${HOME}/gh-pages
+echo -n "Cloning gh-pages branch to ${GH_PAGES_DIR}.. "
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/lupino3/edumips64.git ${GH_PAGES_DIR} > /dev/null
+echo "done."
+
+echo "Git branch: $TRAVIS_BRANCH"
+
+# Execute special per-branch actions.
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  # Build JAR.
+  echo -n "Building JAR.. "
+  ant latest-jar > /dev/null
+  echo "done."
+  cp edumips64-latest.jar ${GH_PAGES_DIR}
+
+  # Deploy the last version of the web UI.
+  deploy_webui ${GH_PAGES_DIR}
+elif [ "$TRAVIS_BRANCH" == "webui-prototype" ]; then
+  echo -n "Copying web UI... "
+  mkdir -p ${GH_PAGES_DIR}/webui
+  rm -rf ${GH_PAGES_DIR}/webui/*
+  cp -Rf $HOME/webui/* ${GH_PAGES_DIR}/webui
+  echo "done."
+fi
+
+# Add, commit and push files.
+cd ${GH_PAGES_DIR}
+git add -f .
+echo "Git status:"
+git status
+if ! git diff-index --quiet HEAD; then
+  echo -n "Committing files.. "
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+  echo "done."
 fi
