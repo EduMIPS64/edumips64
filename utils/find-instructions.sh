9e8a8220e5e952e242ba61fdab1d86bad369746f
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-02-17 10:55:46 +0000

    Script to find untested instructions

---
+++ utils/find-instructions.sh
@@ -0,0 +1,15 @@
+TESTDIR="src/test/resources"
+TESTED_FILENAME="/tmp/tested-instructions$(date +%F-%T).txt"
+ISDIR="src/main/java/org/edumips64/core/is"
+IS_FILENAME="/tmp/instructions-$(date +%F-%T).txt"
+UNTESTED_FILENAME="/tmp/untested-instructions-$(date +%F-%T).txt"
+
+find ${TESTDIR} -name "*.s" | xargs cat | sed "s/\s/ /g" | sed "s/^\s*//g" | sed "s/^.*://g" | cut -d " " -f 1 | grep -v "^[.;#/\\]" | tr "a-z" "A-Z" | grep -v "^$" | sort -u > ${TESTED_FILENAME}
+echo "Tested instructions: $(wc -l ${TESTED_FILENAME})"
+
+grep -ri class.*extends src/main/java/org/edumips64/core/is/ | grep -v Exception | grep -v abstract | sed "s/^.*class//" | cut -d" " -f 2 | sed "s/_/./g" | sort -u > ${IS_FILENAME}
+echo "Total instructions in the instruction set: $(wc -l ${IS_FILENAME})"
+
+diff -u ${IS_FILENAME} ${TESTED_FILENAME} | grep "^-" | grep -v -- "^---" | grep -v -- "- " | sed "s/^-//" > ${UNTESTED_FILENAME}
+echo "Untested instructions: $(wc -l ${UNTESTED_FILENAME})"
+cat ${UNTESTED_FILENAME}
