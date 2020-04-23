ec7b70edd43743e89c390aea50893bc6166a89a6
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2020-04-23 22:17:06 +0100

    Small Web UI improvements (#331)
    * Remove spurious line from sample
    
    * Use monospaced font for components
    
    * Make Code a functional component
    
    * Fix HTML IDs
    
    Remove unused IDs and add back one which was mistakenly removed

--- src/main/java/org/edumips64/client/ui.js
+++ src/main/java/org/edumips64/client/ui.js
@@ -11,48 +11,36 @@ const Registers = (props) => {
 const Statistics = (props) => {
     return (
         <div className="pure-u-1 pure-u-lg-1-4">
-            <textarea id="statistics" readOnly value={props.stats} />
+            <textarea readOnly value={props.stats} />
         </div>
     );
 }
 const Memory = (props) => {
     return (
         <div className="pure-u-1 pure-u-lg-1-4">
-            <textarea id="memory" readOnly value={props.memory} />
+            <textarea readOnly value={props.memory} />
         </div>
     );
 }
 
-class Code extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    handleChange = (event) => {
-        this.props.onChangeValue(event.target.value)
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div className="pure-u-1 pure-u-lg-1-4">
-                <textarea 
-                    value={this.props.code}
-                    onChange={this.handleChange}
-                    />
-                <br />
-                <input type="button" value="Run" onClick={() => {this.props.onClick()}} />
-            </div>
-        );
-    }
+const Code = (props) => {
+    return (
+        <div className="pure-u-1 pure-u-lg-1-4">
+            <textarea 
+                value={props.code}
+                onChange={(event) => {props.onChangeValue(event.target.value);}}
+                />
+            <br />
+            <input id="run-button" type="button" value="Run" onClick={() => {props.onClick()}} />
+        </div>
+    );
 }
 
 const Simulator = (props) => {
     const [registers, setRegisters] = React.useState("Registers will appear here.");
     const [memory, setMemory] = React.useState("Memory will appear here.");
     const [stats, setStats] = React.useState("Statistics will appear here");
-    const [code, setCode] = React.useState(`
-; Example program. Loads the value 10 (A) into R1.
+    const [code, setCode] = React.useState(`; Example program. Loads the value 10 (A) into R1.
 .data
     .word64 10
 
