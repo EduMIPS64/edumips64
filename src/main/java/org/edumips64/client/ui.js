378d79105f2413a507f7a919fe8c63c4556f3df6
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2020-04-23 22:14:00 +0100

    Make Code a functional component

--- src/main/java/org/edumips64/client/ui.js
+++ src/main/java/org/edumips64/client/ui.js
@@ -23,28 +23,17 @@ const Memory = (props) => {
     );
 }
 
-class Code extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    handleChange = (event) => {
-        this.props.onChangeValue(event.target.value)
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div className="pure-u-1 pure-u-lg-1-4">
-                <textarea 
-                    value={this.props.code}
-                    onChange={this.handleChange}
-                    />
-                <br />
-                <input type="button" value="Run" onClick={() => {this.props.onClick()}} />
-            </div>
-        );
-    }
+const Code = (props) => {
+    return (
+        <div className="pure-u-1 pure-u-lg-1-4">
+            <textarea 
+                value={props.code}
+                onChange={(event) => {props.onChangeValue(event.target.value);}}
+                />
+            <br />
+            <input type="button" value="Run" onClick={() => {props.onClick()}} />
+        </div>
+    );
 }
 
 const Simulator = (props) => {
