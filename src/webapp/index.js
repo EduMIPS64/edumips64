f40f240e66a43df6e0c48977f8536f250b279558
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2020-05-30 10:31:59 +0100

    Add basic Appinsights monitoring to Web UI

--- src/webapp/index.js
+++ src/webapp/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 
 import Simulator from "./components/Simulator";
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+// Initialize AppInsights.
+const appInsights = new ApplicationInsights({ config: {
+  instrumentationKey: '4fdd6b3c-15fb-4fd2-910e-0bd297b8d293'
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView(); 
+console.log("Initialized AppInsights");
+
 // Web Worker that runs the EduMIPS64 core, built from the Java codebase.
 // Contains some syntactical sugar methods to make working with the
 // Web Worker API a bit easier.
