07c6a2e46f0150700a13108b503f1a7164844064
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2020-06-04 21:32:18 +0100

    feat(web): highlight filled pipeline stages (#421)
    * feat(web): highlight filled pipeline stages
    
    * fix(web): add FPMultiplier7 stage
    
    * fix(web): fixed width for instruction name field

--- src/webapp/components/Pipeline.js
+++ src/webapp/components/Pipeline.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Stage = ({ name, stage, alwaysDisplay = false }) => {
-  if (!stage && !alwaysDisplay) {
+const Stage = ({ name, instruction, cssClass, alwaysDisplay = false }) => {
+  const isBubble = instruction?.Name === " ";
+  const shouldDisplay = instruction || alwaysDisplay;
+  const shouldHighlight = !!instruction && !isBubble;
+  if (!shouldDisplay) {
     return <React.Fragment />;
   }
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{stage?.Code}</td>
+    <tr className={shouldHighlight ? cssClass : ""}>
+      <td className="instructionName">{name}</td>
+      <td>{instruction?.Code}</td>
     </tr>
   );
 };
@@ -17,22 +20,23 @@ const Pipeline = ({ pipeline }) => {
     <div id="pipeline">
       <table>
         <tbody>
-          <Stage name="IF" stage={pipeline.IF} alwaysDisplay={true} />
-          <Stage name="ID" stage={pipeline.ID} alwaysDisplay={true} />
-          <Stage name="EX" stage={pipeline.EX} alwaysDisplay={true} />
-          <Stage name="MEM" stage={pipeline.MEM} alwaysDisplay={true} />
-          <Stage name="WB" stage={pipeline.WB} alwaysDisplay={true} />
-          <Stage name="FPU Divider" stage={pipeline.FPDivider} />
-          <Stage name="FPU Adder 1" stage={pipeline.FPAdder1} />
-          <Stage name="FPU Adder 2" stage={pipeline.FPAdder2} />
-          <Stage name="FPU Adder 3" stage={pipeline.FPAdder3} />
-          <Stage name="FPU Adder 4" stage={pipeline.FPAdder4} />
-          <Stage name="FPU Multiplier 1" stage={pipeline.FPMultiplier1} />
-          <Stage name="FPU Multiplier 2" stage={pipeline.FPMultiplier2} />
-          <Stage name="FPU Multiplier 3" stage={pipeline.FPMultiplier3} />
-          <Stage name="FPU Multiplier 4" stage={pipeline.FPMultiplier4} />
-          <Stage name="FPU Multiplier 5" stage={pipeline.FPMultiplier5} />
-          <Stage name="FPU Multiplier 6" stage={pipeline.FPMultiplier6} />
+          <Stage name="IF" instruction={pipeline.IF} alwaysDisplay={true} cssClass="stageIf"/>
+          <Stage name="ID" instruction={pipeline.ID} alwaysDisplay={true} cssClass="stageId"/>
+          <Stage name="EX" instruction={pipeline.EX} alwaysDisplay={true} cssClass="stageEx"/>
+          <Stage name="MEM" instruction={pipeline.MEM} alwaysDisplay={true} cssClass="stageMem"/>
+          <Stage name="WB" instruction={pipeline.WB} alwaysDisplay={true} cssClass="stageWb"/>
+          <Stage name="FPU Divider" instruction={pipeline.FPDivider} cssClass="stageFPDivider"/>
+          <Stage name="FPU Adder 1" instruction={pipeline.FPAdder1} cssClass="stageFPAdder"/>
+          <Stage name="FPU Adder 2" instruction={pipeline.FPAdder2} cssClass="stageFPAdder"/>
+          <Stage name="FPU Adder 3" instruction={pipeline.FPAdder3} cssClass="stageFPAdder"/>
+          <Stage name="FPU Adder 4" instruction={pipeline.FPAdder4} cssClass="stageFPAdder"/>
+          <Stage name="FPU Multiplier 1" instruction={pipeline.FPMultiplier1} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 2" instruction={pipeline.FPMultiplier2} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 3" instruction={pipeline.FPMultiplier3} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 4" instruction={pipeline.FPMultiplier4} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 5" instruction={pipeline.FPMultiplier5} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 6" instruction={pipeline.FPMultiplier6} cssClass="stageFPMultiplier"/>
+          <Stage name="FPU Multiplier 7" instruction={pipeline.FPMultiplier7} cssClass="stageFPMultiplier"/>
         </tbody>
       </table>
     </div>
