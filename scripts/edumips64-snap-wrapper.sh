e2dc7d3fe52a1c4e28563672bd5dc49b98ea0aa6
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2020-10-22 18:30:40 +0100

    fix: update jar name in snap wrapper

--- scripts/edumips64-snap-wrapper.sh
+++ scripts/edumips64-snap-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar -Djava.util.prefs.userRoot="$SNAP_USER_DATA" $SNAP/jar/edumips64-1.2.7.1.jar
+java -jar -Djava.util.prefs.userRoot="$SNAP_USER_DATA" $SNAP/jar/edumips64-1.2.8.jar
