681e24310c5354ef375890bcb861f67dbaf604c9
Paolo Viotti <pviotti@users.noreply.github.com>
2020-10-26 17:30:41 +0000

    feat: decompress manual from jar

--- src/main/java/org/edumips64/ui/swing/GUIHelp.java
+++ src/main/java/org/edumips64/ui/swing/GUIHelp.java
@@ -1,7 +1,6 @@
 /* GUIHelp.java
  *
  * EduMIPS64 Help
- * (c) 2006 Vanni Rizzo G.
  *
  * This file is part of the EduMIPS64 project, and is released under the GNU
  * General Public License.
@@ -30,6 +29,20 @@ import java.awt.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,14 +51,18 @@ import javax.help.HelpBroker;
 import javax.help.HelpSet;
 import javax.help.HelpSetException;
 
+
+import org.edumips64.utils.CurrentLocale;
+
 /**
  * This class controls the Edumips64 user guide.
  */
 public class GUIHelp {
   private final static String HELPSET = "EduMIPS64.hs";
   private final static Logger log = Logger.getLogger(GUIHelp.class.getName());
+  private static Path manTmpDir;
 
-  /**
+   /**
    * Shows the EduMIPS64 help window.
    *
    * In case of error, logs a SEVERE error statement and returns without showing the help window.
@@ -93,5 +110,42 @@ public class GUIHelp {
     helpBroker.initPresentation();
     helpBroker.setFont(newFont);
     helpBroker.setDisplayed(true);
+  }
+
+  private static void decompressManual() throws IOException, URISyntaxException {
+      manTmpDir = Files.createTempDirectory("edumips64-");
+      var helpDir = CurrentLocale.getString("HELPDIR");
+      copyFromJar(helpDir + File.separator + "html", manTmpDir);
+  }
+
+  private static void copyFromJar(String source, final Path target) throws URISyntaxException, IOException {
+    var resourceUri = GUIHelp.class.getResource(source).toURI();
+    final Path sourcePath;
+    if (resourceUri.toString().startsWith("file:")) {  // executing from IDE
+      sourcePath = Path.of(resourceUri.toString().replace("file:", ""));
+    } else {                                        // executing from jar
+      var fileSystem = FileSystems.newFileSystem(
+              resourceUri,
+              Collections.<String, String>emptyMap()
+      );
+      sourcePath = fileSystem.getPath(source);
+    }
+
+    Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
+        private Path currentTarget;
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            currentTarget = target.resolve(sourcePath.relativize(dir).toString());
+            Files.createDirectories(currentTarget);
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Files.copy(file, target.resolve(sourcePath.relativize(file).toString()), StandardCopyOption.REPLACE_EXISTING);
+            return FileVisitResult.CONTINUE;
+        }
+    });
   }
 }
