6270e2e606ebb22f7ea66ca4cc8a395d043b8592
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2022-02-21 21:12:32 +0100

    fix: use $SNAP_ARCH to locate jre

--- scripts/edumips64-snap-wrapper.sh
+++ scripts/edumips64-snap-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$SNAP/usr/lib/jvm/java-17-openjdk-amd64/bin/java -jar -Djava.util.prefs.userRoot="$SNAP_USER_DATA" $SNAP/jar/edumips64-1.2.9.jar $*
+$SNAP/usr/lib/jvm/java-17-openjdk-$SNAP_ARCH/bin/java -jar -Djava.util.prefs.userRoot="$SNAP_USER_DATA" $SNAP/jar/edumips64-1.2.9.jar $*
