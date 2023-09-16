1aa0a582e9e3edf7e380d11e620752a9d8c32ddb
YC L <smallg0at@outlook.com>
2023-09-16 23:03:52 +0800

    Remove zh docs pdf upload action

--- src/main/java/org/edumips64/utils/CurrentLocale.java
+++ src/main/java/org/edumips64/utils/CurrentLocale.java
@@ -720,7 +720,7 @@ public class CurrentLocale {
     zhcn.put("HT.MultipleFile", "错误：-f只能使用一次。");
     zhcn.put("CLEAR", "清除");
     zhcn.put("DATA", "数据");
-    zhcn.put("HELPDIR", "/docs/user/zh"); //This has been put in place to prevent help feature blowing up.
+    zhcn.put("HELPDIR", "/docs/user/zh"); 
     zhcn.put("NEGADDRERR", "指令中的负内存地址错误");
     zhcn.put("ALIGNERR", "指令中的对齐错误");
     zhcn.put("THEADDRESS", "该地址");
