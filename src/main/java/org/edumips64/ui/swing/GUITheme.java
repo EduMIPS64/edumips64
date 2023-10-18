a13418859df9c6f72e6e553af1c91b49bd92eba8
YC L <smallg0at@outlook.com>
2023-10-18 23:50:31 +0800

    guitheme: add docs

--- src/main/java/org/edumips64/ui/swing/GUITheme.java
+++ src/main/java/org/edumips64/ui/swing/GUITheme.java
@@ -7,10 +7,10 @@ import org.edumips64.utils.ConfigStore;
 import java.awt.*;
 
 public class GUITheme {
-    public static Color lightText = Color.black;
-    public static Color lightBackground = Color.white;
-    public static Color darkText = new Color(187, 187, 187);
-    public static Color darkBackground = new Color(70, 73, 75);
+    private static Color lightText = Color.black;
+    private static Color lightBackground = Color.white;
+    private static Color darkText = new Color(187, 187, 187);
+    private static Color darkBackground = new Color(70, 73, 75);
     
     private static boolean usingDarkTheme;
 
@@ -21,11 +21,12 @@ public class GUITheme {
         this.config = config;
     }
 
-
+    // Make the current color darker.
     private static Color dimColor(Color color){
         return color.darker().darker();
     }
 
+    // Get color from a specific setting key
     public Color getColor(ConfigKey key){
         if(usingDarkTheme){
             return dimColor(new Color(config.getInt(key)));
@@ -34,10 +35,12 @@ public class GUITheme {
         }
     }
 
+    // Get the text color under current theme
     public Color getTextColor(){
         return usingDarkTheme ? darkText : lightText;
     }
 
+    // Get the background color under current theme
     public Color getBackgroundColor(){
         return usingDarkTheme ? darkBackground : lightBackground;
     }
