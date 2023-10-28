0bbcd9ab57a5ede1308dcead5fa8a905bd393bc4
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2023-10-28 21:36:53 +0200

    fix: add UTF8 file encoding option to snap
    Will help with #785

--- scripts/edumips64-snap-wrapper.sh
+++ scripts/edumips64-snap-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$SNAP/usr/lib/jvm/java-17-openjdk-$SNAP_ARCH/bin/java -jar -Djava.util.prefs.userRoot="$SNAP_USER_DATA" $SNAP/jar/edumips64.jar $*
+$SNAP/usr/lib/jvm/java-17-openjdk-$SNAP_ARCH/bin/java -Dfile.encoding=UTF8 -Djava.util.prefs.userRoot="$SNAP_USER_DATA" -jar $SNAP/jar/edumips64.jar $*
