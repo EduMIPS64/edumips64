f78d68f757dd477052293a775d4f50496bdeac3a
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2025-02-09 16:18:16 +0100

    feat: add tooltips

--- src/webapp/components/Header.js
+++ src/webapp/components/Header.js
@@ -6,6 +6,7 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 
 import HelpDialog from './HelpDialog';
 import CpuStatusDisplay from './CpuStatusDisplay';
@@ -35,108 +36,126 @@ export default function Header(props) {
         >
           Web Version
         </Typography>
-        <CpuStatusDisplay status={props.status} />
-        <Button
-
-          color="inherit"
-          className="load-button"
-          id="load-button"
-          onClick={() => {
-            props.onLoadClick(1);
-          }}
-          startIcon={<SyncIcon />}
-          disabled={!props.loadEnabled}
-        >
-          Load / Reset
-        </Button>
-        <Button
-          color="inherit"
-          className="step-button"
-          id="step-button"
-          onClick={() => {
-            props.onStepClick(1);
-          }}
-          startIcon={<PlayArrowIcon />}
-          disabled={!props.stepEnabled}
-        >
-          Single Step
-        </Button>
-        <TextField
-          type="number"
-          size="small"
-          value={multiStepCount}
-          onChange={(e) => {
-            const value = parseInt(e.target.value, 10);
-            if (value > 0 && value <= 10000) {
-              setMultiStepCount(value);
-            }
-          }}
-          disabled={props.stopEnabled}
-          sx={{
-            width: '100px',
-            mx: 1,
-            input: {
-              color: 'inherit'
-            },
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: 'rgba(255, 255, 255, 0.23)',
+        <Tooltip title="The current status of the CPU" arrow placement="top">
+          <div>
+            <CpuStatusDisplay status={props.status} />
+          </div>
+        </Tooltip>
+        <Tooltip title="Loads the current code into the simulator. If a program is running, it resets the simulator and starts from the first cycle" arrow placement="top">
+          <Button
+            color="inherit"
+            className="load-button"
+            id="load-button"
+            onClick={() => {
+              props.onLoadClick(1);
+            }}
+            startIcon={<SyncIcon />}
+            disabled={!props.loadEnabled}
+          >
+            Load / Reset
+          </Button>
+        </Tooltip>
+        <Tooltip title="Runs a single step of simulation" arrow placement="top">
+          <Button
+            color="inherit"
+            className="step-button"
+            id="step-button"
+            onClick={() => {
+              props.onStepClick(1);
+            }}
+            startIcon={<PlayArrowIcon />}
+            disabled={!props.stepEnabled}
+          >
+            Single Step
+          </Button>
+        </Tooltip>
+        <Tooltip title="Number of steps that the 'Multi step' button will execute" arrow placement="top">
+          <TextField
+            type="number"
+            size="small"
+            value={multiStepCount}
+            onChange={(e) => {
+              const value = parseInt(e.target.value, 10);
+              if (value > 0 && value <= 10000) {
+                setMultiStepCount(value);
+              }
+            }}
+            disabled={props.stopEnabled}
+            sx={{
+              width: '100px',
+              mx: 1,
+              input: {
+                color: 'inherit'
               },
-            },
-          }}
-          inputProps={{
-            min: 1,
-            max: 100000,
-            'aria-label': 'Steps count'
-          }}
-        />
-        <Button
-          color="inherit"
-          className="multi-step-button"
-          id="multi-step-button"
-          onClick={() => {
-            props.onStepClick(multiStepCount);
-          }}
-          startIcon={<FastForwardIcon />}
-          disabled={!props.stepEnabled}
-        >
-          Multi Step
-        </Button>
-        <Button
-          color="inherit"
-          className="run-button"
-          id="run-button"
-          onClick={() => {
-            props.onRunClick();
-          }}
-          startIcon={<PlayCircleIcon />}
-          disabled={!props.runEnabled}
-        >
-          Run All
-        </Button>
-        <Button
-          color="inherit"
-          className="stop-button"
-          id="stop-button"
-          onClick={() => {
-            props.onStopClick();
-          }}
-          startIcon={<StopCircleIcon />}
-          disabled={!props.stopEnabled}
-        >
-          Stop
-        </Button>
-        <Button
-          color="inherit"
-          className="clear-code-button"
-          id="clear-code-button"
-          startIcon={<DeleteForeverIcon />}
-          onClick={() => {
-            props.onClearClick();
-          }}
-          disabled={props.status === 'RUNNING'}
-        > Clear Code
-        </Button>
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'rgba(255, 255, 255, 0.23)',
+                },
+              },
+            }}
+            inputProps={{
+              min: 1,
+              max: 100000,
+              'aria-label': 'Steps count'
+            }}
+          />
+        </Tooltip>
+        <Tooltip title="Run multiple steps of simulation" arrow placement="top">
+          <Button
+            color="inherit"
+            className="multi-step-button"
+            id="multi-step-button"
+            onClick={() => {
+              props.onStepClick(multiStepCount);
+            }}
+            startIcon={<FastForwardIcon />}
+            disabled={!props.stepEnabled}
+          >
+            Multi Step
+          </Button>
+        </Tooltip>
+        <Tooltip title="Run until the simulation ends" arrow placement="top">
+          <Button
+            color="inherit"
+            className="run-button"
+            id="run-button"
+            onClick={() => {
+              props.onRunClick();
+            }}
+            startIcon={<PlayCircleIcon />}
+            disabled={!props.runEnabled}
+          >
+            Run All
+          </Button>
+        </Tooltip>
+        <Tooltip title="Stop the simulation" arrow placement="top">
+          <Button
+            color="inherit"
+            className="stop-button"
+            id="stop-button"
+            onClick={() => {
+              props.onStopClick();
+            }}
+            startIcon={<StopCircleIcon />}
+            disabled={!props.stopEnabled}
+          >
+            Stop
+          </Button>
+        </Tooltip>
+        <Tooltip title="Remove all the code from the editor, leaving only an empty assembly file" arrow placement="top">
+          <Button
+            color="inherit"
+            className="clear-code-button"
+            id="clear-code-button"
+            startIcon={<DeleteForeverIcon />}
+            onClick={() => {
+              props.onClearClick();
+            }}
+            disabled={props.status === 'RUNNING'}
+          >
+            Clear Code
+          </Button>
+        </Tooltip>
         <IconButton
           color="inherit"
           className="help-button"
