e2ef5c6900a790ee72aec2979d68da66245aa3a3
davidepatti <xedivad@gmail.com>
2025-03-26 16:10:25 +0100

    Added electron related script

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  // Se hai una webapp che gira su un server locale (npm start),
+  // qui potresti puntare all'URL http://localhost:8080
+  // Oppure, se vuoi tutto offline, punta direttamente ai file HTML
+  mainWindow.loadURL('http://localhost:8080');
+  
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
