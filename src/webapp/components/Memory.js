c0e48e81dcac3d9d8fc84ed2f8cd35838ec8225d
davidepatti <xedivad@gmail.com>
2025-03-27 19:56:45 +0100

    Renamed exported component to match its use

--- src/webapp/components/Memory.js
+++ src/webapp/components/Memory.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
-const Memory = (props) => {
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+
+
+// Component that shows a binary value.
+const BinaryValue = ({ hexString, value }) => {
   return (
-      <textarea readOnly value={props.memory} id="memory-view"/>
+      <Tooltip disableFocusListener title={value ?? ''}>
+        <Typography sx={{
+          fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+          padding: '0.1em 10px',
+          textAlign: 'right',
+        }}>
+          {hexString}
+        </Typography>
+      </Tooltip>
   );
+};
+
+
+const MemoryElement = ({memoryelement}) => {
+  return (
+      <>
+          <td className="elementName">{memoryelement.address}</td>
+      </>
+  );
+};
+
+const Memory = ({ cells = [], instructions =[]}) => {
+    return (
+        <div>
+            <table id="memory">
+                <tbody>
+                {
+                    cells.map((memoryelement, i) => (
+                        <tr key={i}>
+                            <MemoryElement memoryelement={memoryelement} />
+                            <MemoryElement memoryelement={cells[i]} />
+                        </tr>
+                    ))
+                }
+                {
+                    instructions.map((memoryelement, i) => (
+                        <tr key={i}>
+                            <MemoryElement memoryelement={memoryelement} />
+                            <MemoryElement memoryelement={instructions[i]} />
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default Memory;
