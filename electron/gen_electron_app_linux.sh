2a1f1550848cd9622aecd2fcf1db7720155a5abc
davidepatti <xedivad@gmail.com>
2025-04-03 14:18:01 +0200

    Updated Linux and Windows scripts to adjust file paths and clean up.

--- electron/gen_electron_app_linux.sh
+++ electron/gen_electron_app_linux.sh
@@ -1,3 +1,5 @@
 #npx electron-packager . MyApp --platform=linux --arch=x64 --icon=path/to/myicon.png --out dist
-npx electron-packager . WebEdumips64 --platform=linux --arch=x64 --out dist
-
+rm -rf ./dist/WebEduMips64-darwin-arm64
+cp index.js ..
+npx electron-packager ../ WebEdumips64 --platform=linux --arch=x64 --out dist
+rm ../index.js
--- electron/gen_electron_app_win32.sh
+++ electron/gen_electron_app_win32.sh
@@ -1,3 +1,5 @@
 #npx electron-packager . MyApp --platform=win32 --arch=x64 --icon=assets/myicon.ico --out dist
-npx electron-packager . WebEdumips64 --platform=win32 --arch=x64 --out dist
-
+rm -rf ./dist/WebEduMips64-darwin-arm64
+cp index.js ..
+npx electron-packager ../ WebEdumips64 --platform=win32 --arch=x64 --out dist
+rm ../index.js
