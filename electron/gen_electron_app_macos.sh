532e6599ce336a7a6058ca646c95c2187c84109b
davidepatti <xedivad@gmail.com>
2025-04-04 10:10:14 +0200

    Enable `--asar` in Electron build scripts.
    Added the `--asar` flag to macOS and Windows build scripts to package application files into an archive for improved performance and security. No other functionality was affected.

--- electron/gen_electron_app_macos.sh
+++ electron/gen_electron_app_macos.sh
@@ -1,4 +1,4 @@
 rm -rf ./dist/WebEduMips64-darwin-arm64
 cp index.js ..
-npx electron-packager ../ WebEduMips64 --platform=darwin --arch=arm64 --out dist
+npx electron-packager ../ WebEduMips64 --asar --platform=darwin --arch=arm64 --out dist
 rm ../index.js
--- electron/gen_electron_app_win32_powershell.ps1
+++ electron/gen_electron_app_win32_powershell.ps1
@@ -2,5 +2,5 @@
 $arch = "x64"
 Remove-Item -Recurse -Force ./dist/WebEduMips64-darwin-$arch
 Copy-Item index.js ..
-npx electron-packager ../ WebEdumips64 --platform=win32 --arch=$arch --out dist
+npx electron-packager ../ WebEdumips64 --asar --platform=win32 --arch=$arch --out dist
 Remove-Item ../index.js
