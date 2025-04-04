ec6d20880581cc189b85d816a2e27a8d344dc541
davidepatti <xedivad@gmail.com>
2025-04-04 10:11:52 +0200

    Remove obsolete Windows Electron build scripts.
    Deleted unused `gen_electron_app_win32_cmd.sh` and `gen_electron_app_win32_gitbash.sh` scripts as they are no longer needed. This helps clean up the repository and avoid confusion with outdated tooling.

--- electron/gen_electron_app_win32_cmd.sh
+++
@@ -1,6 +0,0 @@
-$arch="x64"
-#$arch="arm64"
-rmdir /s /q dist\WebEduMips64-win32-$arch
-copy index.js ..
-npx electron-packager .. WebEdumips64 --platform=win32 --arch=$arch --out dist
-del ..\index.js
--- electron/gen_electron_app_win32_gitbash.sh
+++
@@ -1,5 +0,0 @@
-$arch="x64"
-rm -rf ./dist/WebEduMips64-win32-$arch
-cp index.js ..
-npx electron-packager ../ WebEdumips64 --platform=win32 --arch=$arch --out dist
-rm ../index.js
